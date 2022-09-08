64ddfcaa7972570461e87b0fd6595a662778d4eb
Nate Kim <natekim@lawn-143-215-56-178.lawn.gatech.edu>
2022-09-08 14:20:29 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My React App</h1>
+      <h2>Author: Nate Kim</h2>
+    </div>
+  );
+}
+
+export default App;
